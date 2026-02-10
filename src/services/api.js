29e02dbc6a4efc13a3c20aaa664a769a0ee5cf3f
andrespenam <andresenriquee17@gmail.com>
2026-02-10 12:36:09 -0300

    correccion del deploy

--- src/services/api.js
+++ src/services/api.js
@@ -21,7 +21,7 @@ const getErrorMessage = (data, status) => {
 export async function request(path, options = {}) {
     const token = getToken();
 
-    const res = await fetch(`${API_URL}${path}`, {
+    const res = await fetch(`${API_URL}/api${path}`, {
         ...options,
         headers: {
             "Content-Type": "application/json",
